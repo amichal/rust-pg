5afa4d3f8023e398e43656065fb24830f567332b
Aaron Michal <amichal@greenriver.org>
2015-11-01 11:21:00 -0500

    docs

--- README.md
+++ README.md
@@ -9,12 +9,13 @@ git clone git@github.com:amichal/rust-pg.git;
 cd rust-pg;
 cargo run # build any missing our out of date binaries to `target/debug/...` and then run the main package (listed in Cargo.toml)
 # or
-cargo run --release 
+cargo run --release
 ```
 
 # Notes for rails devs
 * `Cargo.toml` is like a `Gemfile` or `.gemspec`. See also `Cargo.lock`. https://crates.io/ ~= https://rubygems.org/
-* `RustConfig` and `Procfile` support  `heroku create --buildpack https://github.com/emk/heroku-buildpack-rust.git` which is used to deploy to heroku. This is currently inefficent as hell as it uploads all the source to heroku and rebuilds all the dependancies from sctrach on each `git push heroku` master. THe buildpack could be enhanced to cache the build directory and then only copy the release binaries etc to the dynos... bit it works for now
+* `RustConfig` and `Procfile` support  `heroku create --buildpack https://github.com/emk/heroku-buildpack-rust.git` which is used to deploy to heroku. This is currently inefficent as hell as it uploads all the source to heroku and rebuilds all the dependancies from sctrach on each `git push heroku` master. THe buildpack could be enhanced to cache the build directory and then only copy the release binaries etc to the dynos... bit it works for now. Basically I followed https://github.com/emk/heroku-rust-cargo-hello
+
 * This currently uses `https://github.com/iron/iron` as a web framework and `https://github.com/sfackler/rust-postgres` for talking to postgres. I want to try out https://github.com/nickel-org/nickel.rs as well and some of the ORMs and db abstraction layers
 * Next up for exploration are HTML templating. https://github.com/jeremyletang/rust-haml exists but since my goal ist o explore more efficent runtime i really want to try https://github.com/lfairy/maud which does the template compliation at runtime and doesn't need to have everything turned into a heap allocated Hash
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
+/// quick hack at
+// library dependancies via Cargo crates
+
 extern crate postgres;
 extern crate iron;
 
+// choose namespaces we need to use.
 use std::io::prelude::*;
 use std::io::{self};
 use std::str::FromStr;
+
 use std::env;
+
 use iron::prelude::*;
 use iron::status;
+
 use postgres::{Connection, SslMode};
 
+
+// a structure mapping to a database row
 struct Person {
     id: i32,
     name: String,
@@ -16,40 +25,66 @@ struct Person {
 }
 
 
+// main entry point.
 fn main() {
+    // setup a iron request handler (notice this is a inner function
+    // of main)
     fn handler(_: &mut Request) -> IronResult<Response> {
+
+        // we will need a mutable buffer to write to
+        // Vec::new() gets is an expandable array of bytes
+        // that supports the Write trate
         let mut buffer = Vec::new();
+
+        // call our funtion with that to fill it up
         pg(&mut buffer);
+
+        // return from our handler a Ok result (rust-wise)
+        // containing a status::Ok http response with our
+        // buffer as the body
         Ok(Response::with((status::Ok, buffer)))
     }
+
+    // bind Iron to all ipv4 interfaces on some port.
+    // in rust we call unwrap() a lot to consume the Result of
+    // any functions that return a Result. this avoids warnings
+    // and helps the compiler know when to free return memory
     Iron::new(handler).http(("0.0.0.0", get_server_port())).unwrap();
 }
 
-/// Look up our server port number in PORT, for compatibility with Heroku.
+/// Look up our server port number in PORT env, for compatibility with Heroku.
 fn get_server_port() -> u16 {
+
+    // this is copy/paste from https://github.com/emk/heroku-rust-cargo-hello. Feels like there might be a more compact way to write it
     let port_str = env::var("PORT").unwrap_or(String::new());
     FromStr::from_str(&port_str).unwrap_or(3000)
 }
 
 /// Get the database url from the environment
 fn get_pg_url() -> String {
-	env::var("DATABASE_URL").unwrap_or("postgres://postgres@localhost".to_string())
+	// pull a database url from the environment or default to localhost
+  env::var("DATABASE_URL").unwrap_or("postgres://postgres@localhost".to_string())
 }
 
 /// Get a postgres database URL.
-
 #[test]
 fn test_pg() {
+  // this is cool. you can write tests in inline and 'feature flag' them to onlu get built in some modes. Include this function and run it with `cargo test`
 	pg(&mut std::io::stdout());
 }
 
+/// Dumping groud and the main body of our code
+// see the docs for rust-postgres https://github.com/sfackler/rust-postgres for the details of the API. Most of this is direct copy paste from there
 fn pg(output: &mut Write) {
     let mut n = 0;
+
+
+    // connect to postgress
     let conn = Connection::connect(&*get_pg_url(), &SslMode::None)
             .unwrap();
 
+  // table creation/population code. which i'm not running right now
   //   conn.execute("DROP TABLE IF EXISTS person;", &[]).unwrap();
-
   //   conn.execute("CREATE TABLE person (
   //                   id              SERIAL PRIMARY KEY,
   //                   name            VARCHAR NOT NULL,
@@ -71,6 +106,8 @@ fn pg(output: &mut Write) {
 		// conn.execute("COMMIT TRANSACTION", &[]).unwrap();
   //   	writeln!(output, "Done inserting {} people", n);
   //   }
+
+    // select prepared statement (which we bind to arguments below)
     let stmt = conn.prepare("SELECT id, name, data FROM person where id < $1").unwrap();
     n = 0;
     for row in stmt.query(&[&1000]).unwrap() {
